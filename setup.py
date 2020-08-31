e7ff3fe5831bbf22cf8d46c075da156e3922853c
Parth Diwanji <diwanji.parth@gmail.com>
2020-08-31 12:05:10 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="ActionRecognition",
+    version="0.0.1",
+    author="Parth Diwanji",
+    author_email="diwanji.parth@gmail.com",
+    description="Deep Learning model for action recognition in videos",
+    url="https://github.com/Parth27/ActionRecognitionVideos",
+    packages=setuptools.find_packages(),
+    license='Apache License 2.0',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['pandas','numpy>=1.6.1','torch==1.5.1','bisect','scipy>=0.9','pip install opencv-python==3.4.4','scikit-learn','glob'],
+)
